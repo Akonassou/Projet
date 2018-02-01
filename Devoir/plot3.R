fa40b6d4bc0a56ccef717570c2ed65f8cbef2cdd
Akonassou <36052609+Akonassou@users.noreply.github.com>
2018-02-01 22:20:29 +0000

    Assignment

--- Devoir/plot3.R
+++ Devoir/plot3.R
@@ -9,3 +9,4 @@ png("plot3.png", width = 480, height = 480)
 ts.plot(ts(data_vrai$Sub_metering_1),ts(data_vrai$Sub_metering_2),ts(data_vrai$Sub_metering_3),ylab="Energy Sub Metering",xlab=" ",col=c("black","red","green"))
 
 dev.off()
+
